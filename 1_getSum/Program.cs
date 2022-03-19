a48fa087e8c76f67563f9dcad22f4bdcec4cd027
beginner4c <100949417+beginner4c@users.noreply.github.com>
2022-03-19 23:55:06 +0900

    Add files via upload
    C#을 C처럼 사용해 함수 사용

---
+++ 1_getSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// C처럼 구현된 C#
+// Procedural Programming 이다 (객체 지향x)
+
+namespace _1_getSum
+{
+    class Program
+    {
+        static int getSum(int n)
+        {
+            int total = 0;
+            int i;
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Please use positive number.");
+                Environment.Exit(-1);
+            }
+
+            for (i = 0; i <= n; i++)
+            {
+                total = total + i;
+            }
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int sum;
+
+            sum = Program.getSum(100);
+            Console.WriteLine("sum = " + sum);
+        }
+    }
+}
