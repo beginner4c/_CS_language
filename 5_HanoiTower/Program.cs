f62f7362f412a6d4500f51a24fccc16ed555371f
beginner4c <100949417+beginner4c@users.noreply.github.com>
2022-03-20 22:43:29 +0900

    Add files via upload
    C#으로 간단하게 만들어진 하노이 타워
    recursive function으로 만들어졌다
    이해가 애매하므로 나중에 콘솔 창에서 다시 구현해 볼 예정

---
+++ 5_HanoiTower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_HanoiTower
+{
+    class Program
+    {   
+        static void moveHanoi(int n, char from, char temp, char to)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(from + " => " + to);
+                return;
+            }
+            moveHanoi(n - 1, from, to, temp);
+            moveHanoi(1, from, temp, to); // 가장 무거운 거
+            moveHanoi(n - 1, temp, from, to);
+        }
+        static void Main(string[] args)
+        {
+            moveHanoi(5, 'A', 'B', 'C');
+        }
+    }
+}
