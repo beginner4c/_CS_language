3e5f5d11c42d624f7a8ef7bb4d027d1251670560
beginner4c <100949417+beginner4c@users.noreply.github.com>
2022-04-08 22:44:17 +0900

    Add files via upload
    Collections 라이브러리에 있는 ArrayList를 사용
    배열의 문제점인 크기 고정을 해결해 라이브러리에 구현되어 있지만 실제로 사용하지 않는 것을 권고

---
+++ 42_Collection_ArrayList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; // type 명시가 불가능하다, 꺼낼 때 type casting이 필요하다
+
+// ArrayList 사용법
+// 배열의 문제점인 크기 고정으로 인한 overflow 발생과 기억장소 낭비를 줄인 것
+
+namespace _42_Collection_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("kim");
+            list.Add("park");
+            list.Add("lee");
+            list.Add("kwon");
+            list.Add("ban");
+
+            // for문으로 ArrayList의 traverse 방법
+            for (int i = 0; i < list.Count; i++) // count는 list 안에 개수를 가지고 있다
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.WriteLine("===");
+
+            // foreach문으로 ArrayList의 traverse 방법
+            foreach (String s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("===");
+
+            // data를 하나하나 access 하는 ArrayList traverse 방법
+            IEnumerator e = list.GetEnumerator();
+
+            while (e.MoveNext())
+            {
+                Console.WriteLine(e.Current);
+            }
+        }
+    }
+}
