1fc9b39d2ac7ceeff25ea8fb46d15d71edb0f585
beginner4c <100949417+beginner4c@users.noreply.github.com>
2022-04-08 22:48:12 +0900

    Add files via upload
    라이브러리에 구현된 Queue를 사용
    이전 Stack과 다른 FIFO 방식이며 이것 역시 현재 사용을 권고하지 않는다
    generic class가 구현된 라이브러리에서 따로 받아 사용하기를 권고

---
+++ 44_Collection_Queue/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; // Queue와 IEnumerator가 포함된 라이브러리
+
+// Queue를 사용해봄
+
+namespace _44_Collection_Queue
+{
+    class Program
+    {
+        class Student
+        {
+            public Student(String name, int total, char grade)
+            {
+                this.name = name;
+                this.total = total;
+                this.grade = grade;
+            }
+
+            String name;
+            int total;
+            char grade;
+
+            public override string ToString()
+            {
+                return "[" + name + ", Total : " + total + ", grade : " + grade + "]";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Queue list = new Queue();
+            list.Enqueue(new Student("kim", 99, 'A')); // Enqueue로 입력
+            list.Enqueue(new Student("park", 20, 'F'));
+            list.Enqueue(new Student("lee", 70, 'C'));
+            list.Enqueue(new Student("kwon", 80, 'B'));
+            list.Enqueue(new Student("ban", 90, 'A'));
+
+            Student st = (Student)list.Dequeue(); // Dequeue의 경우 return type이 object이기에 type casting이 필요하다
+            st = (Student)list.Dequeue();
+
+            Console.WriteLine(st.ToString());
+
+            // foreach문으로 Queue의 traverse 방법
+            foreach (Student s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("===");
+
+            // data를 하나하나 access 하는 Queue traverse 방법
+            IEnumerator e = list.GetEnumerator();
+
+            while (e.MoveNext())
+            {
+                Console.WriteLine(e.Current);
+            }
+        }
+    }
+}
